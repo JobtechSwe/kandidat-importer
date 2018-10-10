0dce75cce7a5a62c83f1d8c856630eaa279f16a4
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-10-10 13:14:05 +0200

    Include resources on build

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ElasticImporters',
-    author='Team Manatee',
+    author='Team Narwhal',
     version='1.0.0',
-    packages=['importers', 'importers.repository', 'importers.platsannons',
-              'importers.kandidat', 'importers.taxonomy', 'importers.auranest'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'valuestore', 'psycopg2-binary', 'elasticsearch', 'zeep', 'cx_Oracle', 'python-dateutil'
+        'valuestore', 'psycopg2-binary', 'elasticsearch', 'zeep', 'cx_Oracle',
+        'python-dateutil'
     ],
+    package_data={'': ['**/platser.json']},
     entry_points={
         'console_scripts': [
             'import-platsannonser = importers.platsannons.main:start',
