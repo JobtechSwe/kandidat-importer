78fdc004b1000a5362a751337a94505e9b799fcd
Markus Sköld <markus.skold@arbetsformedlingen.se>
2018-12-21 10:55:22 +0100

    Adds documentation

--- importers/repository/taxonomy.py
+++ importers/repository/taxonomy.py
@@ -2,9 +2,11 @@ from importers.repository import elastic
 from valuestore.taxonomy import get_term as gt, get_entity as ge
 
 
+# Pass through method (should probably be replaced by actual call to valuestore)
 def get_term(taxtype, taxid):
     return gt(elastic.es, taxtype, taxid)
 
 
+# Pass through method (should probably be replaced by actual call to valuestore)
 def get_entity(taxtype, taxid, not_found_response=None):
     return ge(elastic.es, taxtype, taxid, not_found_response)
