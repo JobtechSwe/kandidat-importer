ee124ea12502b198dc66c0ddb12321182cc124e6
Markus Sköld <markus.skold@arbetsformedlingen.se>
2019-03-05 13:51:33 +0100

    Handle null-id in get_term.

--- importers/repository/taxonomy.py
+++ importers/repository/taxonomy.py
@@ -1,9 +1,15 @@
+import logging
 from importers.repository import elastic
 from valuestore.taxonomy import get_term as gt, get_entity as ge
+
+log = logging.getLogger(__name__)
 
 
 # Pass through method (should probably be replaced by actual call to valuestore)
 def get_term(taxtype, taxid):
+    if not taxid:
+        log.warning("Cannot lookup term for 'None' id.")
+        return None
     return gt(elastic.es, taxtype, taxid)
 
 
